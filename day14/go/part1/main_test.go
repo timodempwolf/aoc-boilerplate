40c209ba67c3f765c2e8f76ba12d32f899872214
Timo Dempwolf <timo.dempwolf@pcg.io>
2023-12-11 12:46:46 +0100

    Some fixes for Go

--- day14/go/part1/main_test.go
+++ day14/go/part1/main_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestTask(t *testing.T) {
-	got, _ := calculate("../example.txt")
+	got, _ := calculate("../../example.txt")
 	want := "4361"
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
